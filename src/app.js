460de695c8d5fd5c680362f6ce8c77c6a6c2e081
amankrsingh2001 <141403170+amankrsingh2001@users.noreply.github.com>
2024-03-24 22:47:17 +0530

    all setup config

--- src/app.js
+++ src/app.js
@@ -13,7 +13,15 @@ app.use(
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.static("public"));
+app.use(cookieParser());
+
+//routes imoport
+
+import userRouter from "./routes/user.routes.js";
+
+
+//router declaration
+app.use("/api/v1/users", userRouter);
 
 export { app };
