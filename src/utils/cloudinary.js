09dadd1f8492d59a06cce33871fbe5abff75534f
amankrsingh2001 <141403170+amankrsingh2001@users.noreply.github.com>
2024-03-28 14:06:01 +0530

    fix all register controiler bugs

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,23 +6,32 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+const localFilePath = `public/temp/download.jpeg`
 
 const uploadOnCloudinary = async (localFilePath) => {
   try {
-    if (!localFilePath) return null
+    if (!localFilePath) return null;
     //upload the file on cloudinary
-  const response =  await cloudinary.uploader.upload(localFilePath,{
-        resource_type:"auto"
-    })
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
     // file has been successfully
-    console.log("File uploaded on cloudinary",response.url)
-    console.log(response)
-    return response
-
+    // console.log("File uploaded on cloudinary",response.url)
+    // console.log("This is the response we wanted to see",response);
+    // console.log(error)
+    console.log(localFilePath)
+    fs.unlink(localFilePath, (error) => {
+      if (error) {
+        console.error('Error deleting file:', error);
+      } else {
+        console.log('File deleted successfully');
+      }
+    });
+    return response;
   } catch (error) {
-    fs.unlinkSync(localFilePath) //remove the locally saved temprory file as the upload operation got failed
+    fs.unlinkSync(localFilePath); //remove the locally saved temprory file as the upload operation got failed
     return null;
   }
 };
 
-export {uploadOnCloudinary}
+export { uploadOnCloudinary };
